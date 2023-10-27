6c2ed32824123a298d81802eca0c99dda7432052
Franco Paredes <143425642+Etiketaa@users.noreply.github.com>
2023-10-26 23:28:46 -0300

    Add files via upload
    Dark mode switch
    Alerts
    Data developer

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const toggle = document.getElementById("toggle")
+toggle.addEventListener("click", function () {
+    document.body.classList.toggle("dark");
+}
+);
+function cartel() { alert("Autor: Franco Paredes \nWeb Developer\nEmail: francoparedes1992@gmail.com\nTeléfono: +542915275183") }
